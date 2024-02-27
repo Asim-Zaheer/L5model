5f62b6422eaeadc6f8c5354fcf21b805edda607d
Asim-Zaheer <iasimzaheer.com>
2024-02-27 11:21:29 +0500

    model

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
 import Layout from "../components/Layout";
 import Head from "next/head";
-
+import App from "@/components/design";
+import SecondDesign from "@/components/design2";
 const Home = () => {
     return (
       <>
         <Head>
-          <title>MEFIC</title>
+          <title>LEVELFIVE</title>
         </Head>
         <Layout>
-         
-      
+         <App/>
+        <SecondDesign/>
         </Layout>
       </>
     );
